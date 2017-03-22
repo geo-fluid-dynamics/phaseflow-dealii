91a755b1c32351f729b9ad811b56c5c72bfa48e2
Alexander G Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-03-22 19:58:50 +0100

    Corrected detail description

--- tests/bug_10_fail.cc
+++ tests/bug_10_fail.cc
@@ -9,8 +9,7 @@
 @brief Test for issue https://github.com/alexanderzimmerman/nsb-pcm/issues/10
 
 @detail 
-    If the bug still exists, then this test will fail to build when
-    attempting to resize a std::vector<dealii::Functions::ParsedFunction<dim>>.
+    If the bug still exists, then this test will seg fault when calling parse_parameters.
 
 */
 int main(int /*argc*/, char** /*argv*/)
