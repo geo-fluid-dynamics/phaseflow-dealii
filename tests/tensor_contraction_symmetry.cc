085408692157697da5e1543354de1f45470bec0b
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-04-06 12:32:31 +0200

    Testing the symmetry of this tensor contraction, which is similar to what's used in the operator c in peclet_system.h

---
+++ tests/tensor_contraction_symmetry.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <deal.II/base/tensor.h>
+#include <deal.II/base/numbers.h>
+
+/*! This test confirms the equivalence of the tensor contractions v*T*w and w*T*v */
+int main(int argc, char* argv[])
+{
+    const unsigned int dim = 2;
+    
+    double epsilon = 1.e-14;
+   
+    dealii::Tensor<1, dim> v;
+    dealii::Tensor<1, dim> w;
+    dealii::Tensor<2, dim> T;
+    
+    for (unsigned int i = 0; i < dim; ++i)
+    {
+        v[i] = i + 1;
+        
+        w[i] = 2.*(i + 1);
+        
+        for (unsigned int j = 0; j < dim; ++j)
+        {
+            T[i][j] = 10*(i + 1) + j + 1;
+        }
+    }
+    
+    double vTw = v*T*w, wTv = w*T*v;
+    
+    assert(dealii::numbers::NumberTraits<double>::abs(vTw - wTv) < epsilon);
+    
+    return 0;
+}
+
