d5f5a5f74a74105a7a86426736d25c42dc79d52a
Alexander G Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-04-10 17:26:45 +0200

    Append to previous commit

--- source/peclet_output.h
+++ source/peclet_output.h
@@ -8,7 +8,7 @@ void Peclet<dim>::write_solution()
     if (this->params.output.write_solution_vtk)
     {
         Output::write_solution_to_vtk(
-            "solution.vtk",
+            "solution-"+Utilities::int_to_string(this->time_step_counter)+".vtk",
             this->dof_handler,
             this->solution);    
     }
